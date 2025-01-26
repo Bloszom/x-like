ddc08f569c59dd2363cf4edce0ea0b49b125f32f
Bloszom <blossom.giane@decagon.dev>
2025-01-26 17:33:03 +0100

    following feed and follow feature for in app users Implementations

--- src/app/api/users/[userId]/followers/route.ts
+++ src/app/api/users/[userId]/followers/route.ts
@@ -1,35 +1,104 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { getPostDataInclude, PostsPage } from "@/lib/types";
-import { NextRequest } from "next/server";
+import { FollowerInfo } from "@/lib/types";
 
-export async function GET(req: NextRequest) {
+export async function GET(
+  req: Request,
+  { params: { userId } }: { params: { userId: string } },
+) {
   try {
-    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+    const { user: loggedInUser } = await validateRequest();
 
-    const pageSize = 10;
-
-    const { user } = await validateRequest();
-
-    if (!user) {
+    if (!loggedInUser) {
       return Response.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    const posts = await prisma.post.findMany({
-      include: getPostDataInclude(user.id),
-      orderBy: { createdAt: "desc" },
-      take: pageSize + 1,
-      cursor: cursor ? { id: cursor } : undefined,
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        followers: {
+          where: {
+            followerId: loggedInUser.id,
+          },
+          select: {
+            followerId: true,
+          },
+        },
+        _count: {
+          select: {
+            followers: true,
+          },
+        },
+      },
     });
 
-    const nextCursor = posts.length > pageSize ? posts[pageSize].id : null;
+    if (!user) {
+      return Response.json({ error: "User not found" }, { status: 404 });
+    }
 
-    const data: PostsPage = {
-      posts: posts.slice(0, pageSize),
-      nextCursor,
+    const data: FollowerInfo = {
+      followers: user._count.followers,
+      isFollowedByUser: !!user.followers.length,
     };
 
     return Response.json(data);
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
+
+export async function POST(
+  req: Request,
+  { params: { userId } }: { params: { userId: string } },
+) {
+  try {
+    const { user: loggedInUser } = await validateRequest();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    await prisma.follow.upsert({
+      where: {
+        followerId_followingId: {
+          followerId: loggedInUser.id,
+          followingId: userId,
+        },
+      },
+      create: {
+        followerId: loggedInUser.id,
+        followingId: userId,
+      },
+      update: {},
+    });
+
+    return new Response();
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params: { userId } }: { params: { userId: string } },
+) {
+  try {
+    const { user: loggedInUser } = await validateRequest();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    await prisma.follow.deleteMany({
+      where: {
+        followerId: loggedInUser.id,
+        followingId: userId,
+      },
+    });
+
+    return new Response();
   } catch (error) {
     console.error(error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
